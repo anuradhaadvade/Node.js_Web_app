717c48d35daa81252816229b1ae67f8b2ba5a38d
anuradhaadvade <80817197+anuradhaadvade@users.noreply.github.com>
2023-08-15 00:33:22 +0530

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,6 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
-var connection = mysql.createPool({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+var conn=mysql.createConnection({host:"anuradha-mp01-dbserver.mysql.database.azure.com", user:"mp01dbuser", password:"LB3jG03iswb2qx1", database:"anuradhamp01DB", port:3306, ssl:{ca:fs.readFileSync("{ca-cert filename}")}});
 
 module.exports = connection;
