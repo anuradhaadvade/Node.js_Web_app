9347002fab08a2893823100124f4cacb96709566
anuradhaadvade <80817197+anuradhaadvade@users.noreply.github.com>
2023-08-15 00:41:11 +0530

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,5 +1,4 @@
 const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
 
 var conn=mysql.createConnection({host:"anuradha-mp01-dbserver.mysql.database.azure.com", user:"mp01dbuser", password:"LB3jG03iswb2qx1", database:"anuradhamp01DB", port:3306, ssl:{ca:fs.readFileSync("{ca-cert filename}")}});
 
